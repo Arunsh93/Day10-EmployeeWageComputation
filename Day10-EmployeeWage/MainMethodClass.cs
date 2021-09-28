5845d3d9aa9bdc9a1af36d18c53e6da51889cf90
Arunsh93 <Arunhosur93@gmail.com>
2021-09-28 11:04:51 +0530

    Welcome to Day10 Emp wage calculation

---
+++ Day10-EmployeeWage/MainMethodClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day10_EmployeeWage
+{
+    class MainMethodClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day10 Employee Wage Calculations");
+        }
+    }
+}
