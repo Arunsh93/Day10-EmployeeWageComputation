18fcc051bfab85f97a7b57b8cac2f3ef9a6d3b68
Arunsh93 <Arunhosur93@gmail.com>
2021-09-28 11:15:56 +0530

    Employee Present or Absent Program

---
+++ Day10-EmployeeWage/UC1_PresentOrAbsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day10_EmployeeWage
+{
+    class UC1_PresentOrAbsent
+    {
+        static void Main(string[] args)
+        {
+            int is_Full_Time = 1;
+            int is_Part_Time = 2;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if(empCheck == is_Full_Time)
+            {
+                Console.WriteLine("Employee Present is for Full Time");
+            }
+            else if(empCheck == is_Part_Time)
+            {
+                Console.WriteLine("Employee is Present for Part Time");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }           
+        }
+    }
+}
