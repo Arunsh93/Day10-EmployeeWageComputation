f5b02315122ea8fdcb4c12196fa9c2e42d535ea0
Arunsh93 <Arunhosur93@gmail.com>
2021-09-28 13:12:25 +0530

    Total Wages for Each Company Using Invoking objects and methods

--- Day10-EmployeeWage/MainMethodClass.cs
+++ Day10-EmployeeWage/MainMethodClass.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Day10_EmployeeWage
 {
@@ -6,7 +8,14 @@ namespace Day10_EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Day10 Employee Wage Calculations");
+            UC9_EmpWageBuilderObject dmart = new UC9_EmpWageBuilderObject("DMART", 20, 20, 100);
+            UC9_EmpWageBuilderObject wipro = new UC9_EmpWageBuilderObject("WIPRO", 20, 20, 100);
+            Console.WriteLine("====================Company-1=========================");
+            dmart.ComputeEmployeeWage();
+            Console.WriteLine(dmart.toString());
+            Console.WriteLine("====================Company-2=========================");
+            wipro.ComputeEmployeeWage();
+            Console.WriteLine(wipro.toString());
         }
     }
 }
